0bfe674cf73bca43051107d7813ae0d79fa44afb
bluesight <bluesight@10.0.156.40>
2018-01-16 19:42:23 +0700

    Master

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -16,7 +16,7 @@ public class HelloGit {
    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Input your name : ");
+        System.out.println("Please, Input your name : ");
         String name = input.nextLine();
         System.out.println("Hello "+name);
     }
